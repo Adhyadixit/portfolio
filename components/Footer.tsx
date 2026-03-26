601e29e411a8ce6151eb385d1e89319c2bf1d669
barclaysgifting-rgb <barclaysgifting@gmail.com>
2026-03-26 14:57:22 +0530

    icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,6 +24,19 @@ export default function Footer() {
                             A privately governed principal investment office originating, underwriting, and
                             structuring long-term capital solutions.
                         </p>
+                        <div className="footer__social">
+                            <a 
+                                href="https://www.linkedin.com/company/nabrel" 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                className="footer__social-link"
+                                aria-label="LinkedIn"
+                            >
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                                </svg>
+                            </a>
+                        </div>
                     </div>
 
                     <div>
@@ -41,14 +54,6 @@ export default function Footer() {
                         <div className="footer__col-title">Inquiries</div>
                         <div className="footer__links">
                             <Link href="/contact" className="footer__link">Engage With Us</Link>
-                            <a 
-                                href="https://www.linkedin.com/company/nabrel" 
-                                target="_blank" 
-                                rel="noopener noreferrer" 
-                                className="footer__link"
-                            >
-                                LinkedIn
-                            </a>
                         </div>
                         <div className="footer__col-title" style={{ marginTop: '2rem' }}>Locations</div>
                         <div className="footer__links">
